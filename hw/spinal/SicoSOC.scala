2c4de00970da143bf628dc2688c05ae2538026f7
Vereena Inara <vereena.inara@gmail.com>
2024-10-21 17:22:04 -0400

    Add LICENSE and README.md

--- hw/spinal/SicoSOC.scala
+++ hw/spinal/SicoSOC.scala
@@ -9,19 +9,14 @@ import Util._
 
 case class SicoSOC(initial_memory: Option[Array[Short]]) extends Component {
     val io = new Bundle {
-        val uart_wdata = out(Bits(8 bits))
-        val uart_rdata = in(Bits(8 bits))
-	    val uart_txe   = in(Bool())
-	    val uart_rxf   = in(Bool())
-	    val uart_wr    = out(Bool())
-	    val uart_rd    = out(Bool())
+        val uart = new UartBus
     }
     import io._
 
 
-    uart_wdata := 0
-    uart_wr    := False
-    uart_rd    := False
+    uart.wdata := 0
+    uart.wr    := False
+    uart.rd    := False
 
 
     val cfg = SicoConfig(
@@ -42,7 +37,7 @@ case class SicoSOC(initial_memory: Option[Array[Short]]) extends Component {
     }
 
 
-       
+    
 }
 
 
